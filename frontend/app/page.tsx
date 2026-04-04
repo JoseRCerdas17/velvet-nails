45e61a26c136993accec93b6910f26b7074c5fa5
JoseRCerdas17 <jose12roberto17@gmail.com>
2026-04-04 00:28:23 -0600

    feat: seccion de servicios con 9 servicios

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/layout/Navbar";
 import Hero from "./components/sections/Hero";
+import Servicios from "./components/sections/Servicios";
 import Footer from "./components/layout/Footer";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main>
       <Navbar />
       <Hero />
+      <Servicios />
       <Footer />
     </main>
   );
